5e61b71d8b01716017e9181126f1593c6dc33ee8
ocsunny <sunnychou0218@gmail.com>
2016-06-22 18:01:29 -0700

    First unit test and url mapping,dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.http import HttpResponse
 from django.core.urlresolvers import resolve
 from .views import home_page
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpResponse()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-DO lists</list>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+        
