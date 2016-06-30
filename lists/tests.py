0358a6e07640d6aa9c4b802c9ef3b3695fd861ee
ocsunny <sunnychou0218@gmail.com>
2016-06-30 16:37:36 -0700

    refactoring the main page to another seperate page, not significant changes

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from .models import Item
 from django.test import TestCase
 from django.http import HttpResponse, HttpRequest
 from django.core.urlresolvers import resolve
-from .views import home_page
+from .views import home_page, view_list
 from django.template.loader import render_to_string
 
 # Create your tests here.
@@ -17,6 +17,11 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected_html)
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request=HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -37,8 +42,20 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+    '''
+    def test_home_page_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
 
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+    '''
+
+class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -57,12 +74,18 @@ class HomePageTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
-    def test_home_page_display_all_list_items(self):
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response,'list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
